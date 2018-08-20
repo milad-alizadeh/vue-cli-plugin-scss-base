364c1e07b4a170fdbe9884d9821a230345025ab7
Milad Alizadeh <milaadd@gmail.com>
2018-08-20 18:39:34 +0100

    remove webpack config edit

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 const path = require('path');
 
 // eslint-disable-next-line no-unused-vars
-module.exports = (api, projectOptions) => {
-  api.configureWebpack(webpackConfig => {
-    return {
-      resolve: {
-        alias: {
-          scss: path.resolve(__dirname, '../../src/scss/')
-        }
-      }
-    }
-  })
-};
+module.exports = (api, projectOptions) => {};
